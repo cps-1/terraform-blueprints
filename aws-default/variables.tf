e112c73fa4e2ecd031dd9d2cadd4453d8dbe58f1
Oscar Esgalha <oscar@cps1.tech>
2025-11-28 09:29:38 -0300

    chore(aws): reorder variables

--- aws-default/variables.tf
+++ aws-default/variables.tf
@@ -1,13 +1,13 @@
-variable "eks_cluster_name" {
-  description = "The EKS cluster name"
-  type        = string
-  default     = "cps1-main"
-}
-
 variable "aws_region" {
   description = "The AWS region to provision resources"
   type        = string
   default     = "sa-east-1"
+}
+
+variable "eks_cluster_name" {
+  description = "The EKS cluster name"
+  type        = string
+  default     = "cps1-main"
 }
 
 variable "eks_admins" {
